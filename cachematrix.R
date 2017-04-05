24224d15f06da55a5307500f15ce6064018acc4e
Lisaetee <Lisaetee@gmail.com>
2017-04-04 20:52:26 -0400

    Commit to master

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## first function creates x as the matrix set to other environment
 
-## Write a short comment describing this function
+MakeMatrix <- function(x = matrix()) {
+      inv = NULL 
+      set = function() y 
+            x <<- y  ##environment global
+            inv <<- NULL  ##environment global
 
-makeCacheMatrix <- function(x = matrix()) {
-
+      get = function() x
+      setinv = function(inverse) inv <<- inverse 
+      getinv = function() inv
+      list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+## solve the function. If inverse return, if not inverse, solve and return
+cachesolve <-function(x, ...) {
+      inv = x$getinverse()
+      if(!is.null(m)) {  ##check if inverse, if so, return
+            message("getting cached data")
+            return(m)
+      }
+      data = x$get()
+      inv = solve(data, ...)  #if not inverse, solve and return
+      x$setinv(inv)
+      return(inv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##test file
+##
+mat1 <--(x)
+> show(x)
+[,1] [,2] [,3]
+[1,]    2    4    6
+[2,]    1    3    5
+> show(mat1)
+[,1] [,2] [,3]
+[1,]   -2   -4   -6
+[2,]   -1   -3   -5
